4c41092b9c6564168895ec620eac85cbeef96d41
Thiago G. Q. <thiagogarianiq@gmail.com>
2025-03-14 09:05:33 -0300

    first commit

---
+++ format.py
@@ -0,0 +1,8 @@
+import json 
+import os
+from utils.utils import indent_file, update_matched
+
+# indent_file(filename='./json/bics/bics_ss.json')
+# for file in os.listdir('./json/bics'):
+#     indent_file(f"./json/bics/{file}")
+indent_file("./json/raw_data/issues.json")
